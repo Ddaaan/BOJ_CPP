901c75b43756d5ddec89fd680a395088fa85cdec
Ddaaan <de2572@naver.com>
2022-05-01 22:39:31 +0900

    BOJ_CPP_solve (22.05.01)

--- BOJ_1316_그룹단어 체커.cpp
+++ BOJ_1316_그룹단어 체커.cpp
@@ -7,25 +7,27 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
+    int n, count=0, length;
     string str;
-    bool flag = true;
-    int t, count = 0, length, temp;
-    int arr[26] = {0, };
+    bool flag;
+    cin >> n;
 
-    cin >> t;
-    for (int i=0; i<t; i++) {
+    while(n--) {
         cin >> str;
         length = str.size();
-        for (int j=0; j<length-2; j++) {
-            if (str[i]!=str[i+1]) {
-                for (int k=i+2; k<length; k++) {
-                    if (str[j] == str[i]){
-                        break;
-                    }
+        flag = true;
+
+        for (int i=0; i<length; i++) {
+            for (int j=0; j<i; j++) {
+                if (str[i]!=str[i-1] && str[i]==str[j]) { //그룹단어가 아닐때 flag를 false로
+                    flag = false;
+                    break;
                 }
             }
         }
-        count++;
+        if (flag == true) count++;
     }
+    cout << count << "\n";
+    
     return 0;
 }
